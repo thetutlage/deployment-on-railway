c7bd76db002047d3237aee3da28941f135948694
Harminder Virk <virk.officials@gmail.com>
2024-11-05 23:29:42 +0530

    feat: add backendUrl

--- medusa-config.ts
+++ medusa-config.ts
@@ -4,12 +4,13 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
   admin: {
+    backendUrl: process.env.MEDUSA_BACKEND_URL,
     disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
   },
   projectConfig: {
     redisUrl: process.env.REDIS_URL,
     databaseUrl: process.env.DATABASE_URL,
-    workerMode: process.env.MEDUSA_WORKER_MODE! as "shared" | "worker" | "server",
+    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
